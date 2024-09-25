08f58f33815a6b5506aefc48cec51489e07ea894
Alsayed Ali <alsayedaldkhakhni@gmail.com>
2024-09-25 12:29:14 +0300

    Test BasePlusCommissionEmployee class.

--- Book/Exercises/Chapter09/Ex03/BasePlusCommissionTest.java
+++ Book/Exercises/Chapter09/Ex03/BasePlusCommissionTest.java
@@ -19,7 +19,6 @@ public class BasePlusCommissionTest // class declaration.
 		// instantiate an object and assign the values.
 		BasePlusCommissionEmployee emp1 = new BasePlusCommissionEmployee(
 				                              "Alsayed", "Ali", "123-45-6789", 5000.0, 5.0, 1000.0);
-		
 		// print the data.
 		System.out.printf("%s", emp1);
 	}
