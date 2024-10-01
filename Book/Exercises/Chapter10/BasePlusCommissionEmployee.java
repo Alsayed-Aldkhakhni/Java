28c19bc766d41d11f882c4c64fde6001ed034297
Alsayed Ali <alsayedaldkhakhni@gmail.com>
2024-10-01 21:02:51 +0300

    Employee has both fixed salary and a commission of the sales.

---
+++ Book/Exercises/Chapter10/BasePlusCommissionEmployee.java
@@ -0,0 +1,57 @@
+/*
+ * File name: CommissionEmployee.java 
+ * 
+ * Subject  : Class that emulates employees take commissions.
+ * 
+ * Date     : September 30, 2024
+ * 
+ * Author   : Alsayed A. Aldkhakhni
+ * 
+ * Last mod : September 30, 2024
+ * 
+ * */
+
+public class BasePlusCommissionEmployee extends CommissionEmployee // class declaration.
+{
+	// instance variable.
+	private double baseSalary;
+	
+	// 9-arg constructor.
+	public BasePlusCommissionEmployee(String firstName, String lastName, String SSN,
+			                          int month, int day, int year,	double grossSales,
+			                          double commissionRate, double baseSalary)
+	{
+		// invoke the father's constructor to build its own part of the object.
+		super(firstName, lastName, SSN, month, day, year, grossSales, commissionRate);
+		
+		if(baseSalary <= 0) // validate the salary.
+			throw new IllegalArgumentException("Invalid salary, it must be > 0");
+		
+		this.baseSalary = baseSalary;
+	}
+
+	// modify the baseSalary.
+	public void setBaseSalary(double baseSalary)
+	{
+		if(baseSalary <= 0)
+			throw new IllegalArgumentException("Invalid salary, it must be > 0");
+	
+		this.baseSalary = baseSalary; 
+	}
+	
+	// retrieve the value of baseSalary.
+	public double getBaseSalary() { return baseSalary; }
+	
+	// this employee earnings method.
+	@Override
+	public double earnings() { return getBaseSalary() + super.earnings(); }
+	
+	@Override
+	public String toString()
+	{
+		return super.toString() + String.format("Base Salary     : $%,.2f%n", getBaseSalary());
+	}
+	
+}
+
+
