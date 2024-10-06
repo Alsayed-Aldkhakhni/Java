062ca629584831380c1fbb3e6bad42954ff2f940
Alsayed Ali <alsayedaldkhakhni@gmail.com>
2024-10-06 20:46:42 +0300

    Superclass of the rest.

---
+++ Book/Exercises/Chapter10/Ex13/Shape.java
@@ -0,0 +1,19 @@
+/*
+ * File name:  Shape.java
+ * 
+ * Subject  : The abstract base class, parent of all the subclasses.
+ * 
+ * Date     : October 1, 2024
+ * 
+ * Author   : Alsayed A. Aldkhakhni
+ * 
+ * Last mod : October 2, 2024
+ * 
+ * */
+
+
+// abstract superclass.
+public abstract class Shape
+{
+	public abstract double getArea();
+}
