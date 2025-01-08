325aedeebe79efe788b1e487c836362c2978e760
alsayed aldkhakhni <alsayedaldkhakhni@gmail.com>
2025-01-08 22:13:36 +0200

    Clear the body.

--- Project01/03-App/Maverick/src/control/Main.java
+++ Project01/03-App/Maverick/src/control/Main.java
@@ -13,11 +13,6 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-		Task task1 = new Task(1150, 123, "Front End", TaskStatus.InProgress, LocalDate.of(2025, 1, 15),
-							 "Your are assigned to design and implement\n"+
-							 "the fron end of the web site being developed.\n");
-		
-		System.out.printf("%s", task1);
 		
 	}
 	
