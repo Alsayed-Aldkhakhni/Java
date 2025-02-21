095ba70f4ddb1937c9ad4d5c23aa72a131b73a28
Alsayed Aldkhakhni <alsayedaldkhakhni@gmail.com>
2025-02-22 00:00:43 +0200

    Interface that contains the earnings method that must be exist in concrete classes.

---
+++ 03-Staff-Sphere/03-App/Staff-Sphere/src/model/Payable.java
@@ -0,0 +1,16 @@
+//=====================================================================
+// File Name: Payable.java
+// Subject  : Interface Payable that enforce the
+//            Person's children to implements its method.
+// Date     : Feb 21, 2025
+// Author   : Alsayed A. Khaleel
+//=====================================================================
+
+package model;
+
+public interface Payable // interface declaration.
+{
+	String companyName = "Maverick";
+	double earnings();
+}
+
