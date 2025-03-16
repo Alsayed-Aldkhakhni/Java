a75ee197ba130350d24a409b1cf3a31600525b96
Alsayed Aldkhakhni <alsayedaldkhakhni@gmail.com>
2025-03-16 12:02:09 +0200

    Singly Linked List implementation.

---
+++ 04-DataStructures/01-SinglyLinkedList/src/code/Main.java
@@ -0,0 +1,63 @@
+/*
+ * File name: Main.java
+ * 
+ * Subject  : Test our homemade LinkedList class.
+ * 
+ * Date     : March 14, 2025
+ * 
+ * Author   : Alsayed A. Khaleel
+ * 
+ */
+
+package code;
+
+import java.util.Scanner;
+
+public class Main // class declaration.
+{
+	public static void main(String[] args)
+	{
+		try (Scanner input = new Scanner(System.in)) // try-with-resources to avoid memory leaks.
+		{
+			// list of persons.
+			LinkedList<Person> persons = new LinkedList<Person>("List of persons");
+			
+			char ans = 'y';
+
+			do {
+				// read the SSN.
+				System.out.print("Enter SSN [5 digits]: ");
+				String SSN = input.next();
+
+				// read the 1st name.
+				System.out.print("Enter first name: ");
+				String firstName = input.next();
+
+				// read the 2nd name.
+				System.out.print("Enter last name : ");
+				String lastName  = input.next();
+
+				// read the age.
+				System.out.print("Enter the age   : ");
+				int age = input.nextInt();
+
+				// add new node to the list.
+				persons.insertAtEnd(new Person(SSN, firstName, lastName, age));
+
+				// ask the user for more data to insert.
+				System.out.print("\nOne more[y, n]: ");
+				ans = input.next().charAt(0);
+				System.out.println();
+
+			} while(ans == 'y');
+
+			// print the list's content.
+			while(true)
+				System.out.println(persons.removeHead());
+		}
+		catch(EmptyListException | IllegalArgumentException ex)
+		{
+			ex.printStackTrace();
+		}
+	}
+}
